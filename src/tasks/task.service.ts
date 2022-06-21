75e5244a7eb12af7714885704a8e4671171d9ab1
camila-dornas <camiladornas@gmail.com>
2022-06-21 13:20:39 -0300

    feat: Cria função createTask

--- src/tasks/task.service.ts
+++ src/tasks/task.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { tasks } from './data/tasks.data';
+import { Task } from './tasks';
 
 @Injectable()
 export class TaskService {
-  task = tasks; // traz as informações do banco de dados.
+  data = tasks; // traz as informações do banco de dados.
   getAll() {
-    return this.task;
+    return this.data;
   }
 
   getById(id: number) {
-    const getTask = this.task.find((value) => value.id == id);
-    return getTask; // retorna a task do id selecionadao.
+    const getTask = this.data.find((value) => value.id == id);
+    return getTask; // retorna a task do id selecionado.
   }
+
+  createTask(task: Task) {
+    const lastId = this.data[this.data.length - 1].id;
+    task.id = lastId + 1;
+    this.data.push(task);
+    return task;
+  } // Cria a nova tarefa, adiciona o id em sequencia e retorna a tarefa criada.
 }
